e1bb3152b18a8cc26a43e0d62676f8e55778bdea
Jason Ransom <jason.ransom@woven-planet.global>
2022-10-21 12:36:34 -0400

    Added base elements.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  it('renders the calculator background', () => {
+    const { container } = render(<App />);
+
+    expect(container.querySelector('#calculator-background')).toBeInTheDocument();
+  });
+
+  it('renders a calculator frame', () => {
+    const { container } = render(<App />);
+
+    expect(container.querySelector('#calculator-body')).toBeInTheDocument();
+  });
 });
