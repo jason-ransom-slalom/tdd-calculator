7f510f582bc121048dac9f229cdd7a9b0c3c8af8
Jason Ransom <jason.ransom@woven-planet.global>
2022-11-18 13:02:29 -0500

    Added buttons to main calculator body.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,36 +1,53 @@
 import React from 'react';
-import { render, within } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
+import {BUTTON_VALUE_LIST} from "./app.constants";
 
 describe('App', () => {
-  it('renders the calculator background', () => {
-    const {container} = render(<App/>);
+    it('renders the calculator background', () => {
+        const {container} = render(<App/>);
 
-    expect(container.querySelector('#calculator-background')).toBeInTheDocument();
-  });
-
-  it('renders a calculator frame', () => {
-    const {container} = render(<App/>);
-
-    expect(container.querySelector('#calculator-body')).toBeInTheDocument();
-  });
-
-  it('renders a ResultsWindow within the calculator body', () => {
-    const {container} = render(<App/>);
-
-    // container.getElementsByTagName('p')[0]
-    const calculatorBody = container.querySelector('#calculator-body') as HTMLElement;
-    expect(calculatorBody.getElementsByTagName('p')[0]).toBeInTheDocument();
-  });
-
-  describe('calculator frame', () => {
-    it.skip('positions results window at the top of the frame', () => {
-      const {container} = render(<App/>);
-
-      const calculatorBody = container.querySelector('#calculator-body') as HTMLElement;
-      expect(calculatorBody).toHaveStyle(' background-color: black');
+        expect(container.querySelector('#calculator-background')).toBeInTheDocument();
     });
-  })
 
-})
+    it('renders a calculator frame', () => {
+        const {container} = render(<App/>);
 
+        expect(container.querySelector('#calculator-body')).toBeInTheDocument();
+    });
+
+    it('renders a ResultsWindow within the calculator body', () => {
+        const {container} = render(<App/>);
+
+        // container.getElementsByTagName('p')[0]
+        const calculatorBody = container.querySelector('#calculator-body') as HTMLElement;
+        expect(calculatorBody.getElementsByTagName('p')[0]).toBeInTheDocument();
+    });
+
+    describe('calculator frame', () => {
+        it.skip('positions results window at the top of the frame', () => {
+            const {container} = render(<App/>);
+
+            const calculatorBody = container.querySelector('#calculator-body') as HTMLElement;
+            expect(calculatorBody).toHaveStyle(' background-color: black');
+        });
+    });
+
+    describe('calculator button array', () => {
+        it('should contain 20 buttons', () => {
+            const {getAllByRole} = render(<App/>);
+            const buttons = getAllByRole('button');
+            expect(buttons.length).toEqual(20);
+        });
+
+        it('should contain 1 of each button', () => {
+            const arrayOfExpectedButtons = BUTTON_VALUE_LIST;
+            const {getByText} = render(<App/>);
+
+            arrayOfExpectedButtons.forEach(buttonText => {
+                // ignoring blank button text values
+                buttonText ?? getByText(buttonText);
+            });
+        });
+    });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import ResultsWindow from './components/ResultsWindow/ResultsWindow';
 import CalculatorButton from "./components/CalculatorButton/CalculatorButton";
+import {BUTTON_VALUE_LIST} from "./app.constants";
 
 function App() {
 
@@ -9,13 +10,19 @@ function App() {
 
     }
 
+    const returnCalculatorButton = () =>
+        BUTTON_VALUE_LIST.map(buttonValue =>
+            <CalculatorButton value={buttonValue} clickCalculatorButton={clickCalculatorButton}/>
+        );
+
     return (
         <div id='calculator-background' className='background'>
             <div id='calculator-body' className='calculator'>
-                <ResultsWindow />
+                <ResultsWindow/>
                 <div id='button-body'>
-                    <CalculatorButton value={1} clickCalculatorButton={clickCalculatorButton}/>
-                    <CalculatorButton value={2} clickCalculatorButton={clickCalculatorButton}/>
+                    {
+                        returnCalculatorButton()
+                    }
                 </div>
             </div>
         </div>
